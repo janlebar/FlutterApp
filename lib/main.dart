0a126d8beb7199054392e7da760be04503b0fad0
JAN <janstefanlebar@gmail.com>
2024-02-24 21:19:19 +0100

    portfolio

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to my Flutter App!',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
