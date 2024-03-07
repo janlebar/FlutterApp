8018647a23a6da56548b09c596f795b2d774448a
JAN <janstefanlebar@gmail.com>
2024-03-07 13:18:27 +0100

    carussel works yet images need id assign dynamically

--- lib/caurosel.dart
+++ lib/caurosel.dart
@@ -1,45 +1,105 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'dart:async';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
 
-class Carousel extends StatelessWidget {
-  final List<String> imagePaths = [
-    'assets/A.jpg',
-    'assets/B.jpg',  
-    // Add more image paths as needed
-  ];
+class Carousel extends StatefulWidget {
+  @override
+  _CarouselState createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  int _currentIndex = 0;
+  List<String> imagePaths = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadAssets();
+  }
+
+  Future<void> loadAssets() async {
+    try {
+      final manifestContent = await rootBundle.loadString('AssetManifest.json');
+      final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+      final imagePaths = manifestMap.keys
+          .where((String key) => key.contains('assets/'))
+          .toList();
+      setState(() {
+        this.imagePaths = imagePaths;
+      });
+    } catch (e) {
+      print('Error loading asset paths: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      itemCount: imagePaths.length,
-      options: CarouselOptions(
-        aspectRatio: 9 / 16,
-        viewportFraction: 0.2,
-        initialPage: 0,
-        enableInfiniteScroll: true,
-        reverse: false,
-        autoPlay: true,
-        autoPlayInterval: Duration(seconds: 3),
-        autoPlayAnimationDuration: Duration(milliseconds: 800),
-        autoPlayCurve: Curves.fastOutSlowIn,
-        enlargeCenterPage: true,
-        scrollDirection: Axis.horizontal,
-      ),
-      itemBuilder: (BuildContext context, int index, int realIndex) {
-        final imagePath = imagePaths[index];
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => DetailPage(imagePath: imagePath)),
-            );
-          },
-          child: Image.asset(
-            imagePath,
-            fit: BoxFit.cover,
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+IconButton(
+  icon: Icon(Icons.arrow_back),
+  onPressed: () {
+    setState(() {
+      _currentIndex = (_currentIndex - 1) % imagePaths.length;
+      if (_currentIndex < 0) {
+        _currentIndex = imagePaths.length - 1;
+      }
+    });
+  },
+),
+
+            Expanded(
+              child: Container(
+                height: 700, // Set your preferred height
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: imagePaths.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailPage(
+                              imagePath: imagePaths[index],
+                            ),
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          imagePaths[index],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+IconButton(
+  icon: Icon(Icons.arrow_forward),
+  onPressed: () {
+    setState(() {
+      _currentIndex = (_currentIndex + 1) % imagePaths.length;
+    });
+  },
+),
+
+          ],
+        ),
+        Container(
+          child: Text(
+            'Image ${_currentIndex + 1} of ${imagePaths.length}',
+            style: TextStyle(fontSize: 16),
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 }
--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,25 +1,53 @@
-import 'package:flutter/material.dart';
+import 'dart:convert'; // Import the dart:convert library
 
-class Gallery extends StatelessWidget {
-  final List<String> imagePaths = [
-    'assets/A.jpg',
-    'assets/B.jpg',
-    // Add more image paths as needed
-  ];
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+class Gallery extends StatefulWidget {
+  @override
+  _GalleryState createState() => _GalleryState();
+}
+
+class _GalleryState extends State<Gallery> {
+  List<String> imagePaths = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadAssets();
+  }
+
+  Future<void> loadAssets() async {
+    try {
+      final manifestContent = await rootBundle.loadString('AssetManifest.json');
+      final Map<String, dynamic> manifestMap = json.decode(manifestContent); // Use json object from dart:convert
+
+      final Iterable<String> imagePathsIterable = manifestMap.keys.where((String key) => key.contains('assets/') && key.contains('.jpg'));
+
+      setState(() {
+        imagePaths = imagePathsIterable.toList();
+      });
+    } catch (e) {
+      print('Error loading asset manifest: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // You can adjust the number of grid columns here
+        crossAxisCount: 4, // Adjusted to display 4 images in a row
         crossAxisSpacing: 5.0,
         mainAxisSpacing: 5.0,
       ),
       itemCount: imagePaths.length,
       itemBuilder: (BuildContext context, int index) {
-        return Image.asset(
-          imagePaths[index],
-          fit: BoxFit.cover,
+        return Padding(
+          padding: EdgeInsets.all(10.0), // Add 10px padding around the image
+          child: Image.asset(
+            imagePaths[index],
+            fit: BoxFit.cover,
+          ),
         );
       },
     );
